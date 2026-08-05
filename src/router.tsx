ed5b44ae5a726a52d185956474656ceb8f5719fd
Dildar123Hussain <91930548+Dildar123Hussain@users.noreply.github.com>
2026-08-05 16:09:37 +0530

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,13 +4,16 @@ import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
-
+  console.log("routeTree =", routeTree);
+  
   const router = createRouter({
     routeTree,
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
+  console.log("router =", router);
+  console.log("router.stores =", (router as any).stores);
 
   return router;
 };
